37dd32cee7caf7faf7cfda7beac00cc377710e86
Lâm Hoàng Thắng <102605739+lht2k1238@users.noreply.github.com>
2024-05-16 22:21:25 +0700

    Delete main directory

--- main/java/com/example/cinema_client/constants/Api.java
+++
@@ -1,6 +0,0 @@
-package com.example.cinema_client.constants;
-
-public class Api {
-//    public static String baseURL="https://cinema-be.herokuapp.com/";
-    public static String baseURL="http://localhost:8080";
-}
